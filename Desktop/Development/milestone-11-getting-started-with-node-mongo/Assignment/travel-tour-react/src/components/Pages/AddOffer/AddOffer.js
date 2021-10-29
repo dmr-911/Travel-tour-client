70a30a13c12bac4b5a9992c0ee7409a3ea85692a
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-30 04:12:31 +0600

    Add offer page created and updated

--- Desktop/Development/milestone-11-getting-started-with-node-mongo/Assignment/travel-tour-react/src/components/Pages/AddOffer/AddOffer.js
+++ Desktop/Development/milestone-11-getting-started-with-node-mongo/Assignment/travel-tour-react/src/components/Pages/AddOffer/AddOffer.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Container, Form, Button, Col } from 'react-bootstrap';
 
 const AddOffer = () => {
     return (
-        <div>
-            <h2>This add offer page</h2>
-        </div>
+      <Container fluid className="bg-dark pb-5">
+        <h2 className="text-white mb-2">Add New Offer</h2>
+        <div className="divider bg-info rounded mb-3 mx-auto"></div>
+        <Col xs={10} md={4} className="mx-auto">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="Offer Name"
+                className="border border-1 border-dark"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="Image Url"
+                className="border border-1 border-dark"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="number"
+                placeholder="Enter Cost"
+                className="border border-1 border-dark"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="Enter Rating"
+                className="border border-1 border-dark"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="text"
+                placeholder="Status"
+                className="border border-1 border-dark"
+              />
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Add Offer
+            </Button>
+          </Form>
+        </Col>
+        <div className="divider bg-info rounded mt-5 mx-auto"></div>
+      </Container>
     );
 };
 
